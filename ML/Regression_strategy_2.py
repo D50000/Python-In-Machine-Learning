d82fba5ac46f04e87dd9c7110ad5fea79dd75479
Eddie Hsu <aoe102198@gmail.com>
2021-01-05 15:49:25 +0800

    update new post fapi
    https://fapi.binance.com/fapi/v1/klines?symbol=ETHUSDT&interval=1h&limit=1500

--- ML/Regression_strategy_2.py
+++ ML/Regression_strategy_2.py
@@ -1,6 +1,7 @@
 # ============================================================
 # node-binance-api
-# https://binance-docs.github.io/apidocs/spot/en/#market-data-endpoints
+# https://binance-docs.github.io/apidocs/futures/en/#market-data-endpoints
+# https://fapi.binance.com/fapi/v1/klines?symbol=ETHUSDT&interval=1h&limit=1500
 # ============================================================
 # Copyright 2020, Eddie Hsu (D50000)
 # Released MySelf
