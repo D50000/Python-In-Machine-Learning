e4b52550a5fb4186c2f29bba81f5f0d5b44a7003
D5000 <aoe102198@gmail.com>
2021-01-17 20:17:23 +0800

    update comments

--- ML/Classification_strategy_2.py
+++ ML/Classification_strategy_2.py
@@ -3,7 +3,7 @@
 # https://binance-docs.github.io/apidocs/futures/en/#market-data-endpoints
 # https://fapi.binance.com/fapi/v1/klines?symbol=ETHUSDT&interval=1h&limit=1500
 # ============================================================
-# Copyright 2020, Eddie Hsu (D50000)
+# Copyright 2021, Eddie Hsu (D50000)
 # Released MySelf
 # ============================================================
 
