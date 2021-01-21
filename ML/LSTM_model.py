699dd76914488e4274108870edc1cf411d10978f
D5000 <aoe102198@gmail.com>
2021-01-21 23:03:32 +0800

    Create new train model

---
+++ ML/LSTM_model.py
@@ -0,0 +1,8 @@
+# ============================================================
+# node-binance-api
+# https://binance-docs.github.io/apidocs/futures/en/#market-data-endpoints
+# https://fapi.binance.com/fapi/v1/klines?symbol=ETHUSDT&interval=1h&limit=1500
+# ============================================================
+# Copyright 2021, Eddie Hsu (D50000)
+# Released MySelf
+# ============================================================
