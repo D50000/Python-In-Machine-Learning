1630e6e8a59ecc8659ab35682c826e3802170622
D5000 <aoe102198@gmail.com>
2021-01-22 23:05:13 +0800

    update crawler

--- data/dataset_crawler.py
+++ data/dataset_crawler.py
@@ -104,6 +104,5 @@ df_numpy = {
     }
 df = pd.DataFrame(data=df_numpy)
 # df.index = pd.to_datetime(df.date.astype(np.str))
-price = df['Close']
 print (df)
 df.to_csv("eth_5m.csv", index_label="index")
