67e2ad402e1e26d90122365d32639540e0705d62
D5000 <aoe102198@gmail.com>
2021-01-22 22:10:46 +0800

    add LSTM model

--- ML/LSTM_model.py
+++ ML/LSTM_model.py
@@ -1,8 +1,20 @@
 # ============================================================
 # node-binance-api
-# https://binance-docs.github.io/apidocs/futures/en/#market-data-endpoints
-# https://fapi.binance.com/fapi/v1/klines?symbol=ETHUSDT&interval=1h&limit=1500
+
 # ============================================================
 # Copyright 2021, Eddie Hsu (D50000)
 # Released MySelf
 # ============================================================
+
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, LSTM, TimeDistributed, RepeatVector
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+import matplotlib.pyplot as plt
+
+def readTrain():
+  train = pd.read_csv("../data/eth_5m.csv")
+  return train
