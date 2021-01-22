797a859f62be2e63560a24ef1b073309a1c998b9
D5000 <aoe102198@gmail.com>
2021-01-23 01:40:00 +0800

    add LSTM one-to-one model

--- ML/LSTM_model.py
+++ ML/LSTM_model.py
@@ -34,6 +34,12 @@ def buildTrain(train, pastDay=30, futureDay=5):
 		Y_train.append(np.array(train.iloc[i+pastDay:i+pastDay+futureDay]["Close"]))
 	return np.array(X_train), np.array(Y_train)
 
+def shuffle(X, Y):
+	np.random.seed(10)
+	randomList = np.arange(X.shape[0])
+	np.random.shuffle(randomList)
+	return X[randomList], Y[randomList]
+
 def splitData(X, Y, rate):
 	X_train = X[int(X.shape[0]*rate):]
 	Y_train = Y[int(Y.shape[0]*rate):]
@@ -41,13 +47,34 @@ def splitData(X, Y, rate):
 	Y_val = Y[:int(Y.shape[0]*rate)]
 	return X_train, Y_train, X_val, Y_val
 
+def buildOneToOneModel(shape):
+	model = Sequential()
+	model.add(LSTM(10, input_length=shape[1], input_dim=shape[2],return_sequences=True))
+	# output shape: (1, 1)
+	model.add(TimeDistributed(Dense(1)))    # or use model.add(Dense(1))
+	model.compile(loss="mse", optimizer="adam")
+	model.summary()
+	return model
+
+
 
 
 data = readTrain()
 train_norm = normalize(data)
 print (train_norm)
-# build Data, use last 30 days to predict next 5 days
-X_train, Y_train = buildTrain(train_norm, 30, 5)
-
+# build Data, use last 1 days to predict next 1 days
+X_train, Y_train = buildTrain(train_norm, 1, 1)
+# shuffle the data, and random seed is 10
+X_train, Y_train = shuffle(X_train, Y_train)
 # split training data and validation data
 X_train, Y_train, X_val, Y_val = splitData(X_train, Y_train, 0.1)
+
+# from 2 dimmension to 3 dimension
+Y_train = Y_train[:,np.newaxis]
+Y_val = Y_val[:,np.newaxis]
+
+model = buildOneToOneModel(X_train.shape)
+callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+model.fit(X_train, Y_train, epochs=1000, batch_size=128, validation_data=(X_val, Y_val), callbacks=[callback])
+
+
