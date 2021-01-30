7ccad3950a02d5e21e7ed6c656757c38be0f7ef9
D5000 <aoe102198@gmail.com>
2021-01-30 23:20:16 +0800

    export and save the model to 'model.h5'

--- ML/LSTM_model.py
+++ ML/LSTM_model.py
@@ -8,6 +8,7 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, LSTM, TimeDistributed, RepeatVector
 from keras.layers.normalization import BatchNormalization
@@ -24,8 +25,11 @@ def normalize(train):
 	train = train.drop(["index"], axis=1)
 	train = train.drop(["date"], axis=1)
 	train = train.drop(["timestamp"], axis=1)
-	train_norm = train.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
-	return train_norm
+	# train_norm = train.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	train_norm = scaler.fit_transform(train)
+	train_norm = pd.DataFrame(train_norm, columns = ['Open_Time','Open','High','Low','Close','Volume','Total','Order','Taker_Volume','Taker_Total'])
+	return train_norm, scaler
 
 def buildTrain(train, pastDay=30, futureDay=5):
 	X_train, Y_train = [], []
@@ -69,7 +73,7 @@ def buildManyToOneModel(shape):
 
 
 data = readTrain()
-train_norm = normalize(data)
+train_norm, scaler = normalize(data)
 print (train_norm)
 # build Data, use last 36 days to predict next 1 days
 X_train, Y_train = buildTrain(train_norm, 36, 1)
@@ -94,5 +98,33 @@ plt.xlabel('Epoch')
 plt.legend(['loss', 'val_loss'], loc='upper left')  
 plt.show()
 
+# PREDICTION
+Predict_X_test = model.predict(X_test)
+print(type(Predict_X_test))
+print("+++++++++++++++++++++++++++++++++++++++++++++")
+# Convert dataframe
+# Predict_X_test = np.append(Predict_X_test, Predict_X_test, axis=1)
+# Predict_X_test = pd.DataFrame(Predict_X_test, columns = ['Close'])
+print(type(Predict_X_test))
+print(Predict_X_test)
+Y_test = pd.DataFrame(Y_test, columns = ['Close'])
+# scaler.inverse
+Predict_X_test = scaler.inverse_transform(Predict_X_test)
+Y_test = scaler.inverse_transform(Y_test)
+print(Predict_X_test.tolist())
+print(Y_test.tolist())
+plt.plot(Predict_X_test.tolist())  
+plt.plot(Y_test.tolist())  
+plt.title('Predict Y vs Y')  
+plt.ylabel('time')  
+plt.xlabel('price')  
+plt.legend(['Predict_X_test', 'Y_test'], loc='upper left')  
+plt.show()
 
 # save the train model
+# from keras.models import model_from_json
+# json_string = model.to_json() with open("model.config", "w") as text_file:    
+# text_file.write(json_string)
+# model.save_weights("model.weight")
+from keras.models import load_model
+model.save('model.h5')  # creates a HDF5 file 'model.h5'
