b0e6719490b1baf5454a4e84f8fa0cbb3059e2f0
D5000 <aoe102198@gmail.com>
2021-01-31 17:54:38 +0800

    plt.show the predict value compare with real data

--- ML/LSTM_model.py
+++ ML/LSTM_model.py
@@ -25,17 +25,18 @@ def normalize(train):
 	train = train.drop(["index"], axis=1)
 	train = train.drop(["date"], axis=1)
 	train = train.drop(["timestamp"], axis=1)
-	# train_norm = train.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
+	data_Y = train['Close']
+	# print(data_Y)
 	scaler = MinMaxScaler(feature_range=(0, 1))
 	train_norm = scaler.fit_transform(train)
 	train_norm = pd.DataFrame(train_norm, columns = ['Open_Time','Open','High','Low','Close','Volume','Total','Order','Taker_Volume','Taker_Total'])
-	return train_norm, scaler
+	return train_norm, scaler, data_Y
 
-def buildTrain(train, pastDay=30, futureDay=5):
+def buildTrain(train, data_Y, pastDay=30, futureDay=5):
 	X_train, Y_train = [], []
 	for i in range(train.shape[0]+1-futureDay-pastDay):
 		X_train.append(np.array(train.iloc[i:i+pastDay]))
-		Y_train.append(np.array(train.iloc[i+pastDay:i+pastDay+futureDay]["Close"]))
+		Y_train.append(np.array(train.iloc[i+pastDay:i+pastDay+futureDay]['Close']))
 	return np.array(X_train), np.array(Y_train)
 
 def shuffle(X, Y):
@@ -73,16 +74,16 @@ def buildManyToOneModel(shape):
 
 
 data = readTrain()
-train_norm, scaler = normalize(data)
+train_norm, scaler, data_Y = normalize(data)
 print (train_norm)
 # build Data, use last 36 days to predict next 1 days
-X_train, Y_train = buildTrain(train_norm, 36, 1)
+X_train, Y_train = buildTrain(train_norm, data_Y, 36, 1)
 # split training data and validation data
 X_train, Y_train, X_test, Y_test = splitData(X_train, Y_train, 0.1)
 
 model = buildManyToOneModel(X_train.shape)
 callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
-train_history = model.fit(X_train, Y_train, epochs=500, batch_size=128, validation_split=0.1, callbacks=[callback])
+train_history = model.fit(X_train, Y_train, epochs=1000, batch_size=128, validation_split=0.1, callbacks=[callback])
 
 
 # evaluation
@@ -102,22 +103,54 @@ plt.show()
 Predict_X_test = model.predict(X_test)
 print(type(Predict_X_test))
 print("+++++++++++++++++++++++++++++++++++++++++++++")
-# Convert dataframe
-# Predict_X_test = np.append(Predict_X_test, Predict_X_test, axis=1)
-# Predict_X_test = pd.DataFrame(Predict_X_test, columns = ['Close'])
+# Convert to dataframe and reshape for rescaler
+Predict_X_test = pd.DataFrame(Predict_X_test, columns = ['Close'])
+df_numpy = {
+    'Open_Time': Predict_X_test['Close'],
+	'Open': Predict_X_test['Close'],
+	'High': Predict_X_test['Close'],
+	'Low': Predict_X_test['Close'],
+	'Close': Predict_X_test['Close'],
+	'Volume': Predict_X_test['Close'],
+	'Total': Predict_X_test['Close'],
+	'Order': Predict_X_test['Close'],
+	'Taker_Volume': Predict_X_test['Close'],
+	'Taker_Total': Predict_X_test['Close']
+    }
+df = pd.DataFrame(data=df_numpy)
+Predict_X_test = df
 print(type(Predict_X_test))
-print(Predict_X_test)
+
 Y_test = pd.DataFrame(Y_test, columns = ['Close'])
-# scaler.inverse
+df_numpy2 = {
+    'Open_Time': Y_test['Close'],
+	'Open': Y_test['Close'],
+	'High': Y_test['Close'],
+	'Low': Y_test['Close'],
+	'Close': Y_test['Close'],
+	'Volume': Y_test['Close'],
+	'Total': Y_test['Close'],
+	'Order': Y_test['Close'],
+	'Taker_Volume': Y_test['Close'],
+	'Taker_Total': Y_test['Close']
+    }
+df2 = pd.DataFrame(data=df_numpy2)
+Y_test = df2
+
+# scaler.inverse and extract the 'Close' column
 Predict_X_test = scaler.inverse_transform(Predict_X_test)
 Y_test = scaler.inverse_transform(Y_test)
+print(Predict_X_test[0:10])
+print(Predict_X_test[-10:])
+print(type(Predict_X_test))
+Predict_X_test, Y_test = Predict_X_test[:, 4], Y_test[:, 4]
 print(Predict_X_test.tolist())
 print(Y_test.tolist())
 plt.plot(Predict_X_test.tolist())  
 plt.plot(Y_test.tolist())  
 plt.title('Predict Y vs Y')  
-plt.ylabel('time')  
-plt.xlabel('price')  
+plt.ylabel('price')  
+plt.xlabel('time')  
 plt.legend(['Predict_X_test', 'Y_test'], loc='upper left')  
 plt.show()
 
